996eb5393f655aad090f431589aa1cd9db499d4e
Zachary Inkster <z.j.inkster@gmail.com>
2020-11-24 11:19:10 -0600

    Added 5 test for add function and created add method

--- ErbiumCustomListProj/CustomList.cs
+++ ErbiumCustomListProj/CustomList.cs
@@ -9,5 +9,35 @@ namespace ErbiumCustomListProj
     public class CustomList<T>
     {
         T[] _items;
+        private int count;
+        public int Count
+        {
+            get => count;
+        }
+        private int capacity;
+        public int Capacity
+        {
+            get => capacity;
+        }
+
+        public CustomList()
+        {
+            count = 0;
+            capacity = 4;
+            _items = new T[capacity];
+        }
+        public void Add(T valueToAdd)
+        {
+            if (count < 4)
+            {
+                _items[count] = valueToAdd;
+                count++;
+            }
+            else if(count >= 4)
+            {
+                capacity += capacity;
+                count++;
+            }
+        }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,7 +10,100 @@ namespace UnitTestProject1
         [TestMethod]
         public void Add_PositiveNumbers_CountOfOne()
         {
+            //Arrange
             CustomList<int> myList = new CustomList<int>();
+            int value1 = 5;
+            int expected = 1;
+            int actual;
+            //Act
+            myList.Add(value1);
+            actual = myList.Count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_PositiveNumbers_CountOfFour()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 2;
+            int value3 = 2;
+            int value4 = 2;
+            int expected = 4;
+            int actual;
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            actual = myList.Count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_PositiveNumbers_CapacityOfFour()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 2;
+            int value3 = 2;
+            int value4 = 2;
+            int expected = 4;
+            int actual;
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            actual = myList.Capacity;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_PositiveNumbers_CapacityIncreases()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 2;
+            int value3 = 2;
+            int value4 = 2;
+            int value5 = 2;
+            int expected = 8;
+            int actual;
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            myList.Add(value5);
+            actual = myList.Capacity;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_PositiveNumbers_CapacityDoubles()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 2;
+            int value3 = 2;
+            int value4 = 2;
+            int value5 = 2;
+            int expected = 8;
+            int actual;
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            myList.Add(value5);
+            actual = myList.Capacity;
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
