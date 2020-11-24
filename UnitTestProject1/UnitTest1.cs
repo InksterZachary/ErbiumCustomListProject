d38e795c178803707099a90c2a67ef9edbcbc2a9
Zachary Inkster <z.j.inkster@gmail.com>
2020-11-24 09:29:29 -0600

    New Project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ErbiumCustomListProj;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_PositiveNumbers_CountOfOne()
+        {
+            CustomList<int> myList = new CustomList<int>();
+        }
+    }
+}
